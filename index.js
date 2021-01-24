f420afe35c9a020e4b55afc70236943d0a255db0
hasanaktas <hasanaktas95@gmail.com>
2021-01-24 05:41:08 +0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Sepetteslim Test Api!");
+  res.send("Sepetteslim Test Api by Hasan Aktas!");
 });
 
 app.get("/markets", (req, res) => {
